436c463a8f9d31cfdb6a1e6abd0609bf40536b7b
Toni Jakovcevic <toni.jakovcevic10@gmail.com>
2023-04-04 14:49:47 +0200

    work in progress

---
+++ ShopOnlineSolution/ShopOnlineWeb/Pages/CheckoutBase.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using ShopOnlineModels.Dtos;
+using ShopOnlineWeb.Services.Contracts;
+using ShopOnlineWeb.Shared;
+
+namespace ShopOnlineWeb.Pages
+{
+    public class CheckoutBase:ComponentBase
+    {
+        [Inject]
+        public IJSRuntime Js { get; set; }
+
+        protected IEnumerable<CartItemDto> ShoppingCartItems { get; set; }
+        protected int TotalQuantity { get; set; }
+
+        protected string PaymentDescription { get; set; }
+        protected decimal PaymentAmount { get; set; }
+
+        [Inject]
+        public IShoppingCartService ShoppingCartService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                ShoppingCartItems = await ShoppingCartService.GetItems(HardCoded.UserId);
+                if(ShoppingCartItems != null)
+                {
+                    Guid orderGuid = Guid.NewGuid();
+
+                    PaymentAmount = ShoppingCartItems.Sum(p=>p.TotalPrice);
+                    TotalQuantity = ShoppingCartItems.Sum(p=>p.Quantity);
+                    PaymentDescription = $"O_{HardCoded.UserId}_{orderGuid}";
+
+                }
+            }
+            catch (Exception)
+            {
+                //log exception
+                throw;
+            }
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            try
+            {
+                if(firstRender)
+                {
+                    await Js.InvokeVoidAsync("initPayPalButton");
+                }
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
